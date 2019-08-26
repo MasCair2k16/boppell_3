fe29b0494d69eef7067d54b93e047e04781b099b
Mason Caird <gotgame6@msn.com>
2019-08-26 00:56:28 -0700

    Added INit and server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+//to install dependencies: npm install <pkg> --save
 
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+
+var http = require('http');
+var path = require('path');
+
+const app = express();
+var server = http.createServer(app);
+
+app.use(express.static("static"));
+
+app.get('/', (req,res) => {
+
+    res.sendFile(path.join(__dirname + '/static/'));
+
+});
+
+app.listen(port, () => {console.log(`'http://localhost:${port}`)});
